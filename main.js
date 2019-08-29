c579c630c46cd6975699d9c62e411fa8e2480e66
Sajid16 <sajid.mahboob16@gmail.com>
2019-08-29 10:50:13 +0600

    prime in range modified

--- main.js
+++ main.js
@@ -19,20 +19,11 @@ function check_prime() {
 }
 
 function find_prime_in_range() {
-<<<<<<< HEAD
-    document.getElementById("message").innerHTML = msg;
+    document.getElementById("message").innerHTML = "The prime numbers within the given range are ";
     var range_num1,range_num2, c, d = 0, prime = [];
-    var msg = "The prime numbers within the given range are ";
     range_num1 = document.getElementById("range_num1").value;
     range_num2 = document.getElementById("range_num2").value;
     // console.log(range_num1, range_num2)
-=======
-    var range_num1, range_num2, c, d = 0, prime = [];
-    var msg = "The prime numbers within the given range are ";
-    range_num1 = document.getElementById("range_num1").value;
-    range_num2 = document.getElementById("range_num2").value;
-    document.getElementById("message").innerHTML = msg;
->>>>>>> fdc6adddbaefab241f0bae4972c218b854112d5a
     for (var j = range_num1; j <= range_num2; j++) {
         c = 0;
         for (var i = 2; i <= j / 2; i++) {
